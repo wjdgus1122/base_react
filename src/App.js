0c9328cd40bfda65d9c53705a7f3df9b8046a7fe
wjdgus1122 <qodqodantk@gmail.com>
2022-06-14 13:06:31 +0900

    tag rule

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+// import { Fragment } from "react";
 
 const App = () => {
-  return <div>ddddkdkdkdkdk</div>;
-}
+  return (
+    <>
+      <h1>h 태그!</h1>
+      <h2>h 태그!</h2>
+      {/* <img src="" /> */}
+      {/* <img></img> */}
+    </>
+    // <Fragment>
+    //   <h1>h 태그!</h1>
+    //   <h2>h 태그!</h2>
+    // </Fragment>
+  );
+};
 
 export default App;
