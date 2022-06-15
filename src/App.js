f1a6639f5f0a21dad9784731e2a641f2767423a2
wjdgus1122 <qodqodantk@gmail.com>
2022-06-15 09:57:13 +0900

    props

--- src/App.js
+++ src/App.js
@@ -20,7 +20,17 @@ const App = () => {
       <FnComponents_2 /> */}
       {/* <ClassComonent /> */}
 
-      <FoodMenu />
+      {/* *props
+      =><컴포넌트 프롭스명="값" /> */}
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="콩나물밥" sideMenu="따뜻한물" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="햄버거" sideMenu="콜라" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="포카칩" sideMenu="맥주" />
+      {/* 2일차 */}
     </div>
   );
 };
--- src/components/2_component/FoodMenu.js
+++ src/components/2_component/FoodMenu.js
@@ -1,3 +1,14 @@
-export const FoodMenu = () => {
-  return <h3>오늘의 메뉴는 텐동</h3>;
+import "./../../styles/style.css"; //css경로 적는법
+
+export const FoodMenu = ({ menuName, sideMenu }) => {
+  // console.log(props);
+  // => 프롭스는 오브젝트 형태로 가져오기 때문에 매개변수에 중괄호를 적고 작성할것
+  // console.log(menuName);
+  return (
+    <div>
+      오늘의 메뉴는 <i className="menu">{menuName}</i> 사이드 메뉴는
+      <i className="side">{sideMenu}</i>
+      {/* =>jsx안쪽에 변수를 사용할시 중괄호로 감싸줄것 */}
+    </div>
+  );
 };
