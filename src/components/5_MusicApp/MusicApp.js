4ba7419d5425b18b8c25a8f77d8d41338d8864e2
wjdgus1122 <qodqodantk@gmail.com>
2022-06-20 10:43:13 +0900

    BtnWrap style

--- src/components/5_MusicApp/MusicApp.js
+++ src/components/5_MusicApp/MusicApp.js
@@ -35,7 +35,7 @@ const Section = styled.section`
 `;
 const CoverImg = styled.div`
   width: 90%;
-  height: 280px;
+  height: 250px;
   border-radius: 25px;
   box-shadow: 0 15px 20px 10px rgba(0, 0, 0, 0.3);
   margin: 30px 0;
