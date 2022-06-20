c492482f8a65b984a56f8aa144702d528c99f9fb
wjdgus1122 <qodqodantk@gmail.com>
2022-06-20 10:38:29 +0900

    Playwrap style

--- src/components/5_MusicApp/components/PlayWrap.js
+++ src/components/5_MusicApp/components/PlayWrap.js
@@ -1,8 +1,27 @@
 import styled from "styled-components";
 
-const SPlayWrap = styled.div``;
-const GageWrap = styled.div``;
-const Gage = styled.div``;
+const SPlayWrap = styled.div`
+  width: 100%;
+  margin-top: 30px;
+`;
+const GageWrap = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+`;
+const Gage = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: black;
+`;
+
+const TimeWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 100;
+`;
 
 export const PlayWrap = () => {
   return (
@@ -10,8 +29,10 @@ export const PlayWrap = () => {
       <GageWrap>
         <Gage />
       </GageWrap>
-      <span>00:03</span>
-      <span>03:40</span>
+      <TimeWrap>
+        <span>00:03</span>
+        <span>03:40</span>
+      </TimeWrap>
     </SPlayWrap>
   );
 };
