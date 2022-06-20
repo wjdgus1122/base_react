60168f5cefeb1b2c6f2ffb82b0f403e6ea4ac023
wjdgus1122 <qodqodantk@gmail.com>
2022-06-20 13:36:03 +0900

    PopupMs edit

--- src/components/6_Event/ClickEx.js
+++ src/components/6_Event/ClickEx.js
@@ -18,10 +18,31 @@ const MouseBox = styled.div`
   background-color: ${(props) => props.bgmouseColor};
 `;
 
+const Wrap = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: gray;
+  display: ${(props) => props.dis};
+  justify-content: center;
+  align-items: center;
+`;
+
+const PopMs = styled.div`
+  width: 500px;
+  height: 300px;
+  background-color: white;
+  font-size: 50px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const ClickEx = () => {
   const [color, setcolor] = useState("gold");
   const [bool, SetBool] = useState(true);
   const [mscolor, setMscolor] = useState("salmon");
+  const [distype, setDistype] = useState("none");
   const handleColor = () => {
     if (bool) {
       setcolor("black");
@@ -44,17 +65,29 @@ export const ClickEx = () => {
       SetBool(true);
     }
   };
+  const handlePopMs = () => {
+    if (bool) {
+      setDistype("flex");
+      SetBool(false);
+    } else if (!bool) {
+      setDistype("none");
+      SetBool(true);
+    }
+  };
   return (
     <>
-      <Box bgColor={color} onClick={handleColor}>
+      {/* <Box bgColor={color} onClick={handleColor}>
         클릭해보세요
       </Box>
       <MouseBox
         bgmouseColor={mscolor}
         onMouseEnter={handleMouseOn}
         onMouseLeave={handleMouseLeave}
-      />
-      <button mouse></button>
+      /> */}
+      <button onClick={handlePopMs}>눌러보세요</button>
+      <Wrap dis={distype}>
+        <PopMs>야호오오오오오</PopMs>
+      </Wrap>
     </>
   );
 };
